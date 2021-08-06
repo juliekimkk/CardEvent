9eb5cbfaebdc79743e3b0ecdbcf52f5695d37fa7
DESKTOP-SFTL6JN\82104 <juhee.kim.kr@gmail.com>
2021-08-06 18:22:32 +0900

    버튼 클릭하면 HTML 요소의 value값을 불러와서 clickedCard=[]빈 배열에 넣고 arrNum배열의 인덱스값과 비교하는 if문 만들기. html tag별로 함수 하나씩 만들고 넘기기 적용. 하지만 작동이 잘 되지않음..

---
+++ js/card2.js
@@ -0,0 +1,49 @@
+
+let arrNum = ["0","1","2","3"];
+var clickedCard = [];
+var completedCard = [];
+// function getInputVal(){
+//     var clicked = document.getElementById('inputId1').name;
+//     clickedCard.push(clicked);
+//     console.log(clickedCard);
+// }
+
+
+function getInputVal1(){
+    var clicked = document.getElementById('inputId1').value;
+    clickedCard.push(clicked);
+    console.log(clickedCard);
+    checkNum();
+}
+function getInputVal2(){
+    var clicked2 = document.getElementById('inputId2').value;
+    clickedCard.push(clicked2);
+    console.log(clickedCard);
+    checkNum();
+}
+function getInputVal3(){
+    var clicked3 = document.getElementById('inputId3').value;
+    clickedCard.push(clicked3);
+    console.log(clickedCard);
+    checkNum();
+}
+
+function checkNum(){
+//completedCard에 들어있지않은 카드만. 하기
+
+
+    if(clickedCard[0] == arrNum[0]){
+        alert('matched!');
+        completedCard.push(clickedCard[0]);
+        if(completedCard.indexOf(clickedCard[0])){
+            alert('잘 맞췄습니다. 다음은?');
+        }else{
+            clickedCard.push();
+            console.log(clickedCard);
+        }
+    }else{
+        clickedCard.pop();
+        console.log(clickedCard);
+    }
+}
+
